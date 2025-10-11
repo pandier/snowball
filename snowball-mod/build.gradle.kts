9b034ba517a64bae7438abd64810109bdc66854d
pandier <62116820+pandier@users.noreply.github.com>
2025-10-11 16:23:43 +0200

    feat: Update to 25w41a

--- snowball-mod/build.gradle.kts
+++ snowball-mod/build.gradle.kts
@@ -19,7 +19,7 @@ tasks.processResources {
     filesMatching("fabric.mod.json") {
         expand(
             "version" to project.version,
-            "minecraft_version" to libs.versions.minecraft.get(),
+            "minecraft_version" to libs.versions.minecraft.modversion.get(),
             "loader_version" to libs.versions.fabric.loader.get(),
             "kotlin_loader_version" to libs.versions.fabric.language.kotlin.get()
         )
